dd95997ec97996882364b4bbb02fc1aa960529cb
Dan Sandiford <sonderfjord@gmail.com>
2019-09-24 18:05:10 +0200

    fixes

--- source/material_model/viscoelastic_plastic.cc
+++ source/material_model/viscoelastic_plastic.cc
@@ -445,6 +445,17 @@ namespace aspect
                              Patterns::Bool (),
                              "Whether to apply a stress averaging scheme to account for differences "
                              "between the fixed elastic time step and numerical time step. ");
+          // Temperature in viscosity laws to include an adiabat (note units of K/Pa)
+          prm.declare_entry ("Adiabat temperature gradient for viscosity", "0.0", Patterns::Double(0),
+                             "Add an adiabatic temperature gradient to the temperature used in the flow law "
+                             "so that the activation volume is consistent with what one would use in a "
+                             "earth-like (compressible) model. Default is set so this is off. "
+                             "Note that this is a linear approximation of the real adiabatic gradient, which "
+                             "is okay for the upper mantle, but is not really accurate for the lower mantle. "
+                             "Using a pressure gradient of 32436 Pa/m, then a value of "
+                             "0.3 $K/km$ = 0.0003 $K/m$ = 9.24e-09 $K/Pa$ gives an earth-like adiabat."
+                             "Units: $K/Pa$");
+
         }
         prm.leave_subsection();
       }
@@ -507,11 +518,11 @@ namespace aspect
 
           // Include an adiabat temperature gradient in flow laws
           adiabatic_temperature_gradient_for_viscosity = prm.get_double("Adiabat temperature gradient for viscosity");
-          //if (this->get_heating_model_manager().adiabatic_heating_enabled())
-          //  AssertThrow (adiabatic_temperature_gradient_for_viscosity == 0.0,
-          //               ExcMessage("If adiabatic heating is enabled you should not add another adiabatic gradient"
-          //                          "to the temperature for computing the viscosity, because the ambient"
-          //                          "temperature profile already includes the adiabatic gradient."));
+          if (this->get_heating_model_manager().adiabatic_heating_enabled())
+            AssertThrow (adiabatic_temperature_gradient_for_viscosity == 0.0,
+                         ExcMessage("If adiabatic heating is enabled you should not add another adiabatic gradient"
+                                    "to the temperature for computing the viscosity, because the ambient"
+                                    "temperature profile already includes the adiabatic gradient."));
 
 
 
