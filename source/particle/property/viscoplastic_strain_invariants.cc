07c17975c3b98508423da549ff3fad2b11666f0b
Dan Sandiford <sonderfjord@gmail.com>
2019-09-26 10:18:32 +0200

    fix

--- source/particle/property/viscoplastic_strain_invariants.cc
+++ source/particle/property/viscoplastic_strain_invariants.cc
@@ -106,7 +106,6 @@ namespace aspect
         const MaterialModel::ViscoPlastic<dim> *viscoplastic
           = dynamic_cast<const MaterialModel::ViscoPlastic<dim> *>(&this->get_material_model());
 
-        //dummy vector here
         // note that I'm supplying the composition vector as a substuturte for elastic shear moduli. Just a quick hack
         bool plastic_yielding = false;
         plastic_yielding = viscoplastic->is_yielding(solution[this->introspection().component_indices.pressure],
