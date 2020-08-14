85fe6337ef42b49f2ea81aad19b32118a3e65caf
anne-glerum <a.c.glerum@uu.nl>
2020-08-14 15:25:14 +0200

    Change ConstraintMatrix to AffineConstraints

--- source/mesh_deformation/diffusion.cc
+++ source/mesh_deformation/diffusion.cc
@@ -454,7 +454,7 @@ namespace aspect
     template <int dim>
     void
     Diffusion<dim>::compute_velocity_constraints_on_boundary(const DoFHandler<dim> &mesh_deformation_dof_handler,
-                                                             ConstraintMatrix &mesh_velocity_constraints,
+                                                             AffineConstraints<double> &mesh_velocity_constraints,
                                                              const std::set<types::boundary_id> &boundary_id) const
     {
       if (!apply_diffusion)
